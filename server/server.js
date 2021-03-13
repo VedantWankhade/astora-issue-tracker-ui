fcd98b0bec2ab631674456a8d3d1ede01bd884cf
VedantWankhade <vedantwankhade17@gmail.com>
2021-03-13 22:32:46 +0530

    Created new graphql data type called Issue and created issueList field

--- server/server.js
+++ server/server.js
@@ -4,9 +4,23 @@ const fs = require('fs')
 
 let aboutMessage = "Astora issue tracker API v1.0";
 
+const issuesDB = [
+    {
+        id: 1, status: 'New', owner: 'Ravan', effort: 5,
+        created: new Date('2019-01-15'), due: undefined,
+        title: 'Error in console when clicking Add',
+    },
+    {
+        id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
+        created: new Date('2019-01-16'), due: new Date('2019-02-01'),
+        title: 'Missing bottom border on panel',
+    },
+];
+
 const apiResolvers = {
     Query: {
         about: () => aboutMessage,
+        issueList
     },
     Mutation: {
         setAboutMessage: (_, { message }) => {
@@ -14,6 +28,11 @@ const apiResolvers = {
         },
     },
 };
+
+function issueList() {
+
+    return issuesDB;
+}
 
 const server = new ApolloServer({
     typeDefs: fs.readFileSync('server/schema.graphql', 'utf-8'),
