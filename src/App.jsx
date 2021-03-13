ee0bc752daf5a8b93c6427889780084cda3671f6
VedantWankhade <vedantwankhade17@gmail.com>
2021-03-14 01:25:52 +0530

    Used graphQL variables in queries

--- src/App.jsx
+++ src/App.jsx
@@ -101,22 +101,19 @@ class IssueList extends React.Component {
 
     async createIssue(issue) {
 
-        const query = `mutation {
-                        issueAdd(issue: {
-                            title: "${issue.title}",
-                                owner: "${issue.owner}",
-                                due: "${issue.due.toISOString()}",
-                        }) {
-                            id
-                        }
-                    }`;
+        const query = `mutation issueAdd($issue: IssueInputs!) {
+                            issueAdd(issue: $issue) {
+                                id
+                            }
+                        }`;
+
 
 
 
         const response = await fetch('/api', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify({ query })
+            body: JSON.stringify({ query, variables: { issue } })
         });
         this.loadData();
     }
