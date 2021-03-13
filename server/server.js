932f88af098903b9b77ca3dfb52faa99a1b7e81d
VedantWankhade <vedantwankhade17@gmail.com>
2021-03-13 22:18:35 +0530

    Transfered the graphql typeDefs into schema.graphql file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express')
+const fs = require('fs')
+
+let aboutMessage = "Astora issue tracker API v1.0";
+
+const apiResolvers = {
+    Query: {
+        about: () => aboutMessage,
+    },
+    Mutation: {
+        setAboutMessage: (_, { message }) => {
+            return aboutMessage = message;
+        },
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs: fs.readFileSync('server/schema.graphql', 'utf-8'),
+    resolvers: apiResolvers
+});
+
+
 const app = express();
 const middleware = express.static('public');
+server.applyMiddleware({app, path: '/api'});
 
 app.use('/', middleware);
 
