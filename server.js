fdc8b72d91def67fd5716981eca2d5fb428b824b
VedantWankhade <vedantwankhade17@gmail.com>
2021-03-17 00:53:37 +0530

    Added environment vraibles

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+
+const UI_API_ENDPOINT =
+  process.env.UI_API_ENDPOINT || "http://localhost:3000/api";
+const env = { UI_API_ENDPOINT };
 
 const app = express();
 app.use("/", express.static("public"));
-app.listen(8000, function () {
-  console.log("UI server started at http://localhost:8000");
+
+app.get("/env.js", function (req, res) {
+  res.send(`window.ENV = ${JSON.stringify(env)}`);
+});
+
+const UI_SERVER_PORT = process.env.UI_SERVER_PORT || 8000;
+
+app.listen(UI_SERVER_PORT, function () {
+  console.log(`UI server started at http://localhost:${UI_SERVER_PORT}`);
 });
