8b6785f599af79baa1ad3a90e70018c3fec5ffe6
VedantWankhade <vedantwankhade17@gmail.com>
2021-03-17 01:12:08 +0530

    set proxy for api calls and disable cors

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const express = require("express");
+const proxy = require("http-proxy-middleware");
+
+const apiProxyTarget = process.env.API_PROXY_TARGET;
 
 const UI_API_ENDPOINT =
   process.env.UI_API_ENDPOINT || "http://localhost:3000/api";
@@ -7,6 +10,10 @@ const env = { UI_API_ENDPOINT };
 
 const app = express();
 app.use("/", express.static("public"));
+
+if (apiProxyTarget) {
+  app.use("/api", proxy({ target: apiProxyTarget }));
+}
 
 app.get("/env.js", function (req, res) {
   res.send(`window.ENV = ${JSON.stringify(env)}`);
