3b5dc381b92b936bd270aec76d120e9929e1b614
VedantWankhade <vedantwankhade17@gmail.com>
2021-03-17 16:30:20 +0530

    Added webpack HMR

--- src/App.jsx
+++ src/App.jsx
@@ -5,3 +5,7 @@ import ReactDOM from 'react-dom';
 import IssueList from './IssueList.jsx';
 
 ReactDOM.render(<IssueList />, document.getElementById('contents'));
+
+if (module.hot) {
+  module.hot.accept();
+}
